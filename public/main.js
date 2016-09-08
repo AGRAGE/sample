be35bed5eefdc6fe2549ab1d88d339188a38b11a
Your Name <andreykuleb@mail.ru>
2016-09-08 23:58:06 +0300

    test was changed

--- public/main.js
+++ public/main.js
@@ -22,7 +22,7 @@ function hello (name, numOfVisit){
 	return 'Привет, ' + name + '! Вы авторизировались у нас уже ' + numOfVisit +" " + plural(numOfVisit) + "!";
 }
 
-function plural ( numStr ){
+function plural (numStr){
 	var strangeNum1 = ["12", "13", "14"];
 	var strangeNum2 = ["2", "3", "4"];
 	if (strangeNum1.indexOf(numStr) != -1) return "раз";
